c28f4042bbb282dc3ea4fc78ae7a343b78aa95dc
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-07 14:25:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+  <!-- <RouterLink to="/">Home</RouterLink> -->
+  <!-- <RouterLink to="/about">About</RouterLink> -->
+
+  <RouterView />
+</template>
+
+<style >
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  word-wrap: break-word;
+}
+::-webkit-scrollbar {
+  display: none;
+}
+:root {
+  --glass: #ae36b225;
+  --sixty: #88888800;
+  --bg: #2a51be;
+  --thirty: #006c8d;
+  --acc: #4e0000;
+  --ss: 1%;
+  --se: 1em;
+  --snx: 30vw;
+  --height: 80vh;
+  --width: 70vw;
+  --sny: 40vh;
+  --bubblebg: #cfcbcb;
+  --mute: #00000000;
+  --cursor: #e600ff;
+}
+html {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+/* body {
+  overflow-y: hidden;
+  background-image: radial-gradient(
+    circle,
+    #664cc5,
+    #ad36b2,
+    #dc1491,
+    #f71069,
+    #ff3e3e
+  );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: 100vw 100vh;
+} */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--acc);
+}
+@media (min-width: 1024px) {
+}
+</style>
