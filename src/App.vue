50c8d09ab6635923bed60991a8c5e7840faabee7
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-10 11:16:58 +0200

    'update'

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,71 @@
 <script setup>
-  let i
-  let percentage 
+let i;
+let percentage;
+let score = [];
+let fonts = [
+  "Alkalami",
+  "Anton",
+  "Bitter",
+  "Comforter Brush",
+  "DM Sans",
+  "Gemunu Libre",
+  "Inconsolata",
+  "Indie Flower",
+  "Lato",
+  "Lobster",
+  "Merriweather Sans",
+  "Noto Sans",
+  "Noto Sans Ethiopic",
+  "Noto Serif Display",
+  "Nunito Sans",
+  "Peralta",
+  "PT Serif",
+  "Silkscreen",
+  "cursive",
+  "Titillium Web",
+  "sans-serif",
+  "serif",
+];
 function generateRandomColor() {
-  console.log('clicked')
+  console.log("clicked");
   let letters = "0123456789ABCDEF";
   let color = "#";
-  for ( i = 0; i < 8; i++) {
+  for (i = 0; i < 8; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
-function generateRandomPercentage(){
-percentage=(Math.random()*100)+'%'
-return percentage
+function generateRandomPercentage() {
+  percentage = Math.random() * 100 + "%";
+  return percentage;
 }
-console.log(
-generateRandomPercentage()
-)
-
+console.log(generateRandomPercentage());
 </script>
 
 <template>
-<button v-on:click="generateRandomColor()">click</button>
-<p></p>
+  <div>
+  <div id="score" class="">
+<p>score</p>
+
+  </div>
+  </div>
+  <p></p>
 </template>
 <style>
-
-</style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    word-wrap: break-word;
+  }
+  ::-webkit-scrollbar {
+    display:none
+  }
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48
+  }
+  </style>
